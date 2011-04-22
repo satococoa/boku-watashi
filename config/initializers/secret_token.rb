0ef72609b32a71c1d333206a57a99ed3b563c242
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-04-22 22:35:43 +0900

    rails new boku-watashi -J

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BokuWatashi::Application.config.secret_token = '038462e9c2089587bd88cb3a27925b5840a098c07998f744009777ccb3180f63be18a016fdd9f70cf838f00e0dd4e628baea94839cec23f551f1bb3429374015'
